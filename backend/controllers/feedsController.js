f79d78947bf2613f621f99effd955ab979dd61bc
PriyanshuPr <98030244+Priyanshu-prajapat@users.noreply.github.com>
2025-09-05 03:29:39 +0000

    remove backend.

--- backend/controllers/feedsController.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getAllFeeds, getParticularFeed } from "../services/feedsServices.js";
-
-const router = Router();
-
-router.get("/", getAllFeeds);
-router.get("/:postId", getParticularFeed);
-
-export default router;
